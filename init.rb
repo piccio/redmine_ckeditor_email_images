f0e12d65f03b163e06f43dfce3e3d3d8207ab3f9
Roberto Piccini <roberto.piccini@gmail.com>
2018-04-12 16:26:20 +0200

    bugfix sub-uri & refactoring logger

--- init.rb
+++ init.rb
@@ -1,9 +1,10 @@
+require 'redmine_ckeditor_email_images/ckeditor_email_images_logger'
 require 'redmine_ckeditor_email_images/ckeditor_email_images'
 
 Rails.configuration.to_prepare do
   # plugin does its actions only if ckeditor plugin is present
   if Redmine::Plugin.registered_plugins[:redmine_ckeditor].present?
-    ActionMailer::Base.register_interceptor(CkeditorEmailImages)
+    ActionMailer::Base.register_interceptor(RedmineCkeditorEmailImages::CkeditorEmailImages)
   end
 end
 
@@ -11,8 +12,9 @@ Redmine::Plugin.register :redmine_ckeditor_email_images do
   name 'Redmine Ckeditor Email Images plugin'
   author 'Roberto Piccini'
   description 'insert images in html notification'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/piccio/ckeditor_email_images.git'
   author_url 'https://github.com/piccio'
-  requires_redmine version: '2.6.0'
+
+  settings default: { 'enable_log' => false }, partial: 'settings/ckeditor_email_images'
 end
--- lib/redmine_ckeditor_email_images/ckeditor_email_images.rb
+++ lib/redmine_ckeditor_email_images/ckeditor_email_images.rb
@@ -1,43 +1,52 @@
-# thanks to https://github.com/dkalachov/redmine_email_images
-class CkeditorEmailImages
+module RedmineCkeditorEmailImages
+  class CkeditorEmailImages
 
-  DEBUG = false
-  INVISIBLE_EMAIL_HEADER = '&#8203;' * 20
-  FIND_IMG_SRC_PATTERN = /(<img[^>]+src=")([^"]+)("[^>]*>)/
+    INVISIBLE_EMAIL_HEADER = '&#8203;' * 20
+    FIND_IMG_SRC_PATTERN = /(<img[^>]+src=")([^"]+)("[^>]*>)/
 
-  def self.delivering_email(message)
-    text_part = message.text_part
-    html_part = message.html_part
+    def self.delivering_email(message)
+      text_part = message.text_part
+      html_part = message.html_part
 
-    if html_part
-      related = Mail::Part.new
-      related.content_type = 'multipart/related'
-      related.add_part html_part
-      html_part.body = html_part.body.to_s.gsub(/<body[^>]*>/, "\\0 " << INVISIBLE_EMAIL_HEADER.html_safe)
-      html_part.body = html_part.body.to_s.gsub(FIND_IMG_SRC_PATTERN) do
-        image_url = URI.unescape $2
-        Logger.new("#{Rails.root}/log/ckeditor_email_images.log").info("IMAGE_URL=#{image_url}") if DEBUG
-        attachment_url = image_url
-        if File.exist?(File.join(Rails.public_path, image_url))
-          image_name = File.basename(image_url)
-          Logger.new("#{Rails.root}/log/ckeditor_email_images.log").info("IMAGE_NAME=#{image_name}") if DEBUG
-          related.attachments.inline[image_name] = File.read(File.join(Rails.public_path, image_url))
-          attachment_url = related.attachments[image_name].url
-          Logger.new("#{Rails.root}/log/ckeditor_email_images.log").info("ATTACHMENT_URL=#{attachment_url}") if DEBUG
+      if html_part
+        related = Mail::Part.new
+        related.content_type = 'multipart/related'
+        related.add_part html_part
+        html_part.body = html_part.body.to_s.gsub(/<body[^>]*>/, "\\0 " << INVISIBLE_EMAIL_HEADER.html_safe)
+        html_part.body = html_part.body.to_s.gsub(FIND_IMG_SRC_PATTERN) do
+          img_early_part = $1
+          src_part = $2
+          img_latter_part = $3
+
+          image_url = URI.unescape src_part
+          sub_uri = Rails.configuration.relative_url_root
+          unless sub_uri.blank?
+            image_url = image_url.gsub(/\A#{sub_uri}/, '')
+          end
+          RedmineCkeditorEmailImages::CkeditorEmailImagesLogger.write(:info, "IMAGE URL=#{image_url}")
+          attachment_url = image_url
+          if File.exist?(File.join(Rails.public_path, image_url))
+            image_name = File.basename(image_url)
+            RedmineCkeditorEmailImages::CkeditorEmailImagesLogger.write(:info, "IMAGE NAME=#{image_name}")
+            related.attachments.inline[image_name] = File.read(File.join(Rails.public_path, image_url))
+            attachment_url = related.attachments[image_name].url
+            RedmineCkeditorEmailImages::CkeditorEmailImagesLogger.write(
+              :info, "ATTACHMENT URL=#{attachment_url}")
+          end
+
+          img_early_part << attachment_url << img_latter_part
         end
 
-        $1 << attachment_url << $3
+        # multipart/alternative
+        # - text/plain
+        # - multipart/relative
+        # -- text/html
+        # -- image/*
+        message.parts.clear
+        message.parts << text_part
+        message.parts << related
       end
-
-      # multipart/alternative
-      # - text/plain
-      # - multipart/relative
-      # -- text/html
-      # -- image/*
-      message.parts.clear
-      message.parts << text_part
-      message.parts << related
     end
-  end
 
+  end
 end
